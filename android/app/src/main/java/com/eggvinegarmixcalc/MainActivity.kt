806acd149b0ff8980ec1215179add22daddf0b03
sampss <sampsons79@gmail.com>
2025-07-08 16:32:27 -0400

    updated MainActivity.kt, app still renders on device

--- android/app/src/main/java/com/eggvinegarmixcalc/MainActivity.kt
+++ android/app/src/main/java/com/eggvinegarmixcalc/MainActivity.kt
@@ -1,22 +1,42 @@
 package com.eggvinegarmixcalc
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+// 🧩 AdMob SDK
+import com.google.android.gms.ads.MobileAds
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 class MainActivity : ReactActivity() {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript.
+   * This is used to schedule rendering of the component.
    */
   override fun getMainComponentName(): String = "EggVinegarMixCalc"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * which allows you to enable New Architecture with a single boolean flag [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /**
+   * Initializes the AdMob SDK on a background thread during app launch
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    CoroutineScope(Dispatchers.IO).launch {
+      MobileAds.initialize(this@MainActivity) {
+        // ✅ AdMob SDK is ready; ads can now be safely loaded
+      }
+    }
+  }
 }
